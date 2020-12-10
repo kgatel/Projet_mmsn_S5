393360c1cbb31d6d2ff7a5c6aa5a6367082e5e68
Kévin Gatel <kevin.gatel@insa'rouen.fr>
2020-12-10 21:27:52 +0100

    H sort la mat H + b, ajout du clean dans le makefile du doss H

--- Hilbert/H.f
+++ Hilbert/H.f
@@ -1,16 +1,29 @@
         program Hilbert
         implicit none
         integer i,j,n
-        double precision aij
-        read(*,*)n
-cc        n=5
+        double precision,dimension(:,:), ALLOCATABLE ::A
+        double precision,dimension(:), ALLOCATABLE :: b
+        double precision somme
+
+        read(*,*) n
+        ALLOCATE(A(n,n))
+        ALLOCATE(b(n))
 
         do i=1,n
-        do j=1,n
-        aij = 1.d0/(dble(i)+dble(j)-1.d0)
-        write(*,*)aij 
+        	somme=0
+        	do j=1,n
+        		A(i,j) = 1.d0/(dble(i)+dble(j)-1.d0)
+        		somme=somme+A(i,j)
+        	enddo
+        	b(i)=somme
         enddo
-cc          write(*,*) 
+	
+	
+	do i=1,n
+                write(*,*) (A(i,j),j=1,n)
         enddo
-
+        write(*,*)
+        write(*,*)b
+	
+	
         end
